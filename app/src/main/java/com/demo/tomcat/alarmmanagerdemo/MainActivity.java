bfd365055a2fa2a96a2d669c8296a3fdee548133
tomcat <tomcat.cheng@microlife.com.tw>
2018-04-06 23:55:24 +0800

    init code

---
+++ app/src/main/java/com/demo/tomcat/alarmmanagerdemo/MainActivity.java
@@ -0,0 +1,152 @@
+package com.demo.tomcat.alarmmanagerdemo;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity
+{
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String ACTION_ALARM_SET = "com.demo.tomcat.alarmmanagerdemo.ACTION_ALARM_SET";
+    public static final String ACTION_ALARM_CANCLE = "com.demo.tomcat.alarmmanagerdemo.ACTION_ALARM_CANCLE";
+
+    AlarmReceiver   alarmReceiver;
+    AlarmManager    am;
+    PendingIntent   pi;
+    Calendar        cal;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        Log.w(TAG, "onCreate(), ");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initControl();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        Log.w(TAG, "onStart(), ");
+        super.onStart();
+        if (alarmReceiver == null)
+        {
+            alarmReceiver = new AlarmReceiver();
+            registerReceiver(alarmReceiver, getIntentFilter());
+            Log.w(TAG, " registerReceiver, alarmReceiver: " + alarmReceiver);
+        }
+    }
+
+    @Override
+    protected void onStop()
+    {
+        Log.w(TAG, "onStop(), ");
+        super.onStop();
+
+        if (alarmReceiver != null)
+        {
+            unregisterReceiver(alarmReceiver);
+            Log.e(TAG, " unregisterReceiver alarmReceiver !! ");
+        }
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        Log.w(TAG, "onDestroy(), ");
+        super.onDestroy();
+    }
+
+
+    //------------------ User function ------------------//
+    private void initView()
+    {
+
+    }
+
+    private void initControl()
+    {
+        setAlarm(30);
+    }
+
+    private void setAlarm(int n)
+    {
+        for (int i=0; i<n; i++)
+        {
+            cal = Calendar.getInstance();
+            cal.set(2018, 4-1, 6, 1, 20, 0);
+
+            Intent intent = new Intent(ACTION_ALARM_SET);
+            am = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+            pi = PendingIntent.getBroadcast(this, i, intent, PendingIntent.FLAG_ONE_SHOT);
+            am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pi);
+        }
+
+    }
+
+    private void cancelAlarm(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Intent intent = new Intent(ACTION_ALARM_CANCLE);
+            am = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+            pi = PendingIntent.getBroadcast(this, i, intent, PendingIntent.FLAG_ONE_SHOT);
+            am.cancel(pi);
+
+            pi = null;
+            am = null;
+        }
+    }
+
+    private IntentFilter getIntentFilter()
+    {
+        final IntentFilter filter = new IntentFilter();
+
+        Log.w(TAG, "getIntentFilter(), add action to filter !! ");
+        filter.addAction(ACTION_ALARM_SET);
+
+        return filter;
+    }
+
+
+    //------------------ inner class -------------------//
+    private class AlarmReceiver extends BroadcastReceiver
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            Log.w(TAG, "onReceive(), ");
+            String action = intent.getAction();
+            String message = "";
+
+            if (action.equalsIgnoreCase(ACTION_ALARM_SET))
+            {
+                //Toast.makeText(context, "Times Up!!", Toast.LENGTH_LONG).show();
+                message = "Times Up!!";
+            }
+            else if (action.equalsIgnoreCase(ACTION_ALARM_CANCLE))
+            {
+                //Toast.makeText(context, "Cancel Alarm ~~", Toast.LENGTH_LONG).show();
+                message = "Cancel Alarm ~~";
+            }
+            else
+            {
+                //Toast.makeText(context, "Error !! action unknow.", Toast.LENGTH_LONG).show();
+                message = "Error !! action unknow.";
+            }
+
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+    }
+}
