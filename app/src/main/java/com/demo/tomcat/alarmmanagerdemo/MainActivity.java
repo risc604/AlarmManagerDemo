77c0e356e1af69b48a2418355187ede4cae6bb5c
tomcat <tomcat.cheng@microlife.com.tw>
2018-10-09 11:59:49 +0800

    add notification OK.

--- app/src/main/java/com/demo/tomcat/alarmmanagerdemo/MainActivity.java
+++ app/src/main/java/com/demo/tomcat/alarmmanagerdemo/MainActivity.java
@@ -3,15 +3,24 @@ package com.demo.tomcat.alarmmanagerdemo;
 import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Binder;
 import android.os.Build;
 import android.os.SystemClock;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -44,52 +53,14 @@ public class MainActivity extends AppCompatActivity
 
     AlarmManager    am;
     PendingIntent   pi;
-    AlarmService alarmService;
-    AlarmReceiver   alarmReceiver;
+    //AlarmService alarmService;
+    //AlarmReceiver   alarmReceiver;
 
     //------------------ inner class -------------------//
-    private class AlarmReceiver extends BroadcastReceiver
-    {
-        @SuppressLint("SetTextI18n")
-        @RequiresApi(api = Build.VERSION_CODES.M)
-        @Override
-        public void onReceive(Context context, Intent intent)
-        {
-            String action = intent.getAction();
-            String message = "";
-            Date thisDate = new Date(System.currentTimeMillis());
-            Log.w(TAG, "onReceive(), Action: " + action);
-
-            if (action == null)
-                return;
-
-            if (action.equalsIgnoreCase(ACTION_ALARM_SET))
-            {
-                Log.w(TAG, " timerCounts: " + timerCounts + ", " + sdf.format(thisDate));
-                timerCounts++;
-                message = "Times Up!! " + timerCounts + ", " + sdf.format(thisDate);
-                cancelAlarm(1);
-                startAlarm();
-                tvMessage.setText(timerCounts + ", " + sdf.format(thisDate));
-            }
-            else if (action.equalsIgnoreCase(ACTION_ALARM_CANCEL))
-            {
-                message = "Cancel Alarm ~~";
-                cancelAlarm(1);
-                clearAbortBroadcast();
-                timerCounts = 0;
-            }
-            else
-            {
-                message = "Error !! action unknow.";
-            }
-
-            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-        }
-    }
-
-    //private final BroadcastReceiver alarmReceiver = new BroadcastReceiver() {
+    //private class AlarmReceiver extends BroadcastReceiver
+    //{
     //    @SuppressLint("SetTextI18n")
+    //    @RequiresApi(api = Build.VERSION_CODES.M)
     //    @Override
     //    public void onReceive(Context context, Intent intent)
     //    {
@@ -124,7 +95,48 @@ public class MainActivity extends AppCompatActivity
     //
     //        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     //    }
-    //};
+    //}
+
+    private final BroadcastReceiver alarmReceiver = new BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            String action = intent.getAction();
+            String message = "";
+            Date thisDate = new Date(System.currentTimeMillis());
+            Log.w(TAG, "onReceive(), Action: " + action);
+
+            if (action == null)
+                return;
+
+            if (action.equalsIgnoreCase(ACTION_ALARM_SET))
+            {
+                Log.w(TAG, " timerCounts: " + timerCounts + ", " + sdf.format(thisDate));
+                timerCounts++;
+                message = "Times Up!! " + timerCounts + ", " + sdf.format(thisDate);
+                cancelAlarm(1);
+                notificationDialog(message);
+                startAlarm();
+                tvMessage.setText(timerCounts + ", " + sdf.format(thisDate));
+            }
+            else if (action.equalsIgnoreCase(ACTION_ALARM_CANCEL))
+            {
+                message = "Cancel Alarm ~~";
+                cancelAlarm(1);
+                clearAbortBroadcast();
+                manager.cancelAll();
+                manager = null;
+                timerCounts = 0;
+            }
+            else
+            {
+                message = "Error !! action unknow.";
+            }
+
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+    };
 
 
     @Override
@@ -144,14 +156,12 @@ public class MainActivity extends AppCompatActivity
     {
         Log.w(TAG, "onStart(), ");
         super.onStart();
-        alarmService = new AlarmService();
-        if (alarmReceiver == null)
+        //if (alarmReceiver == null)
         {
             //alarmReceiver = new AlarmReceiver();
             registerReceiver(alarmReceiver, getIntentFilter());
             Log.w(TAG, " registerReceiver, alarmReceiver: " + alarmReceiver);
         }
-        //alarmService = new AlarmService();
     }
 
     @Override
@@ -177,13 +187,11 @@ public class MainActivity extends AppCompatActivity
         super.onDestroy();
         if (alarmReceiver != null)
         {
-            alarmReceiver.clearAbortBroadcast();
+            //alarmReceiver.clearAbortBroadcast();
             unregisterReceiver(alarmReceiver);
             Log.e(TAG, " unregisterReceiver alarmReceiver !! ");
         }
-        cancelAlarm(1);
-        alarmService.onDestroy();
-        alarmService = null;
+        //cancelAlarm(1);
     }
 
     @SuppressLint("SetTextI18n")
@@ -280,6 +288,108 @@ public class MainActivity extends AppCompatActivity
         AlarmService alarmService = new AlarmService();
 
     }
+
+    long AlarmCounts = 0L;
+    NotificationManager manager;
+    public void notificationDialog(String noteMSG)
+    {
+        Log.i(TAG, "notificationDialog(), noteMSG: " + noteMSG +
+                ", AlarmCounts: " + (++AlarmCounts));
+        //if ((appBGFlag && !gattConnectFlag) || (sysSetting.getConnectNotification() == 0))
+        /// if (appBGFlag && !gattConnectFlag)
+        /// {
+        ///     Log.w(TAG, "notificationDialog(), appBGFlag: " + appBGFlag +
+        ///             ", sysSetting.getConnectNotification(): " + sysSetting.getConnectNotification() +
+        ///             ", sysSetting.getNotification()" + sysSetting.getNotification());
+        ///
+        ///     return;
+        /// }
+        ///
+        Intent  mainIntent = MainActivity.this.getIntent();
+        mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        mainIntent.setAction(Intent.ACTION_MAIN);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        //mainIntent.setClass(this, MainActivity.class);
+        //mainIntent.setFlags(  Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP |
+        //                      Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Log.w(TAG, "notificationDialog(), mainIntent: " + mainIntent);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                //mainIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+                mainIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Bitmap bmpIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_launcher_foreground);
+        manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setContentIntent(contentIntent)
+                .setContentTitle(getResources().getString(R.string.app_name))
+                .setContentText(noteMSG)
+                .setWhen(System.currentTimeMillis())
+                //.setColor(Color.parseColor("#ff0000ff"))
+                .setColor(Color.parseColor("#0046ae"))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setLargeIcon(bmpIcon)
+                .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)
+                .setNumber(1)
+                .setChannelId(getString(R.string.app_name))
+                .setVibrate(new long[]{0, 5000, 60*1000, 5000})
+                .setPriority(Notification.PRIORITY_HIGH);
+        //.setAutoCancel(true);
+
+        /// Log.w(TAG, "2 Notification title: " + getResources().getString(R.string.app_name) +
+        ///         ", " + noteMSG + ", lowBatteryFlag: " + lowBatteryFlag +
+        ///         ", gattConnectFlag: " + gattConnectFlag +
+        ///         ", lowBatteryAlarmFlag: " + lowBatteryAlarmFlag);
+
+        //if (gattConnectFlag && !lowBatteryAlarmFlag && !lowBatteryFlag)    // High/Low temperature alarm.
+        /// if (!lowBatteryAlarmFlag && !lowBatteryFlag)    // High/Low temperature alarm.
+        /// {
+            //builder.setDefaults(Notification.DEFAULT_SOUND)
+            builder.setSound(Uri.parse("android.resource://" + getPackageName() +
+                    "/raw/kwahmah_02_alarm1"));
+            ///.setVibrate(new long[]{0L}); // Passing null here silently fails)
+            Log.w(TAG, "Notification Sound Ruuning !!");
+        /// }
+
+        Notification notification = builder.build();
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N_MR1)  // Android 8.0 Notification Channel
+        {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_EVENT).build();
+            @SuppressLint("WrongConstant")
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    getString(R.string.app_name),
+                    noteMSG,
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setShowBadge(true);
+            notificationChannel.setDescription(noteMSG);
+            notificationChannel.setVibrationPattern(new long[]{0, 5000, 60*1000, 5000});
+            //notificationChannel.setSound(Uri.parse("android.resource://" + getPackageName() +
+            //        "/raw/kwahmah_02_alarm1"), audioAttributes);
+            notificationChannel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION), audioAttributes);
+            notificationChannel.setLightColor(getColor(R.color.colorPrimary));
+            notificationChannel.enableVibration(true);
+            //notificationChannel.canShowBadge();
+            manager.createNotificationChannel(notificationChannel);
+            Log.w(TAG, " Android 8.0, Notification Channel!!");
+        }
+        else    // Android normal notification action.
+        {
+            notification.flags |= Notification.FLAG_AUTO_CANCEL;  //sound one shut
+            //manager.notify(10, notification);
+            Log.w(TAG, " Android, Normal Notification ...");
+        }
+        manager.notify(10, notification);
+
+
+        //if (appBGFlag && (Build.VERSION.SDK_INT > Build.VERSION_CODES.N_MR1) ) {
+        //    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        //        startForegroundService((Intent) getSystemService(Context.NOTIFICATION_SERVICE));
+        //    }
+        //}
+        /// int badgeCount = 1;
+        /// ShortcutBadger.applyCount(getBaseContext(), badgeCount); //for 1.1.4+
+    }
+
 
     //private void initAlarm()
     //{
